8db9c1cba79ccd77d552cc408371a2127d211909
Junwei Chen <kngzhi@hotmail.com>
2022-10-12 14:38:48 +0800

    refactor: clean duplicate code

--- src/components/ActionPanel/index.tsx
+++ src/components/ActionPanel/index.tsx
@@ -1,23 +1,20 @@
 import { Text16, Text12, SideText } from '@/components/Base/Text';
 import { BaseCard } from '@/components/Base/Card';
 import styled from 'styled-components';
-import { Flex, FlexCol, FlexRow } from '@/components/Base/Div';
+import { FlexCol, FlexRow } from '@/components/Base/Div';
 import NumericInput from '@/components/NumericInput';
-import { ChevronDown } from 'react-feather';
 // import ActionButton from '@/components/ActionButton';
 import Slider from '@/components/Slider';
 import type { TokenInterface } from '@/constants/tokens';
 import { sUSD, ETH } from '@/constants/tokens';
-import Image from 'next/image';
-import { Text18 } from '@/components/Base/Text';
-import { DefaultDropdownMenu } from '@/components/Dropdown';
+import TokenSelector from '@/components/TokenSelector';
 
 const ActionPanel = () => {
   return (
     <>
       {` `}
       <TokenCard>
-        <TokenSelector token={sUSD} />
+        <TokenSelector activeToken={sUSD} tokenList={[sUSD, ETH]} />
         <Balance>
           <NumericInput value="0.1" placeholder="0.00" />
           <SideText>ETH Balance: 1000</SideText>
@@ -49,54 +46,6 @@ const TokenCard = styled(BaseCard)`
   display: flex;
   justify-content: space-between;
 `;
-
-const TokenSelector = ({ token }: { token: TokenInterface }) => {
-  const CoinSelector = styled(Flex)<{ height?: string }>`
-    align-items: center;
-    gap: 5px;
-    height: ${({ height }) => height || `100%`};
-  `;
-
-  const Coin = styled(Flex)<{ active: boolean }>`
-    height: 44px;
-    align-items: center;
-    padding: 10px 8px;
-    gap: 5px;
-    ${(props) => props.active && `background: rgba(255, 255, 255, 0.06);`}
-  `;
-
-  const DropdownContainer = styled.div`
-    width: 192px;
-    background-color: ${({ theme }) => theme.colors.bgNavy};
-    border: 1px solid #2d2d38;
-    box-shadow: 0px 14px 14px rgba(0, 0, 0, 0.25);
-    border-radius: 5px;
-    z-index: 10;
-  `;
-  return (
-    <DefaultDropdownMenu
-      leftOffset={`-8`}
-      offset={64}
-      trigger={
-        <CoinSelector>
-          <Image src={token.src} alt={token.name} width={34} height={34} />
-          <Text18>{token.name}</Text18>
-          <ChevronDown size={16} />
-        </CoinSelector>
-      }
-      dropList={
-        <DropdownContainer>
-          {[sUSD, ETH].map((token) => (
-            <Coin key={token.key} active={token.key === `susd`}>
-              <Image src={token.src} alt={token.name} width={24} height={24} />
-              <Text18>{token.name}</Text18>
-            </Coin>
-          ))}
-        </DropdownContainer>
-      }
-    />
-  );
-};
 
 const Balance = styled(FlexCol)`
   align-items: flex-end;
